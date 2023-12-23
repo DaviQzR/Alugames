dcfbed9a9e39ad07f43998d551e2045af21fbcf6
DaviQzR <125469425+DaviQzR@users.noreply.github.com>
2023-12-23 16:11:58 -0300

    Adicionando Confirmação para devolver

--- js/app.js
+++ js/app.js
@@ -6,14 +6,16 @@ function alterarStatus(id) {
   // Verifica se o jogo está alugado
   let estaAlugado = imagem.classList.contains('dashboard__item__img--rented');
 
-  // Altera o status de alugado para disponível e vice-versa
+  // Adiciona uma confirmação antes de alterar o status
   if (estaAlugado) {
-      // Se estiver alugado, muda para disponível
-      imagem.classList.remove('dashboard__item__img--rented');
-      botao.textContent = 'Alugar';
-      botao.classList.remove('dashboard__item__button--return');
-  
-    } else {
+      // Se estiver alugado, exibe uma mensagem de confirmação
+      if (confirm("Tem certeza que deseja devolver este jogo?")) {
+          // Se o usuário confirmar, muda para disponível
+          imagem.classList.remove('dashboard__item__img--rented');
+          botao.textContent = 'Alugar';
+          botao.classList.remove('dashboard__item__button--return');
+      }
+  } else {
       // Se estiver disponível, muda para alugado
       imagem.classList.add('dashboard__item__img--rented');
       botao.textContent = 'Devolver';
